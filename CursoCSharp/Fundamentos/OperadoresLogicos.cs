9d9239de911caccb35298a20c3e020ee07bb4c88
Matheus Domingues <59287764+Matheus-D@users.noreply.github.com>
2025-08-11 11:50:31 -0400

    Parei aula 58 - Propriedades - Curso Básico ao avançado

---
+++ CursoCSharp/Fundamentos/OperadoresLogicos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+     class OperadoresLogicos {
+        public static void Executar() {
+            var executouTrabalho1 = false;
+            var executouTrabalho2 = false;
+
+            bool comprouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine("Comprou Tv 50? {0}", comprouTv50);
+
+            var ComprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine("Comprou o sorvete? {0}" , ComprouSorvete);
+
+            var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine("Comprou tv 32? {0}", comprouTv32);
+
+            Console.WriteLine("Mais saudável? {0}", !ComprouSorvete);
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturaDeControle;
+using CursoCSharp.ClassesEMetodos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+
+                // Fundamentos
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentarios - Fundamentos", Comentarios.Executar },
+                {"Variaveis e Constantes - Fundamentos",VariaveisEConstantes.Executar },
+                {"Inferência - Fundamentos",Inferencia.Executar },
+                {"Interpolação - Fundamentos",Interpolacao.Executar },
+                {"Notação Ponto - Fundamentos",NotacaoPonto.Executar },
+                {"Lendo Dados - Fundamentos",LendoDados.Executar },
+                {"Formatando Número - Fundamentos",FormatandoNumero.Executar },
+                {"Conversões - Fundamentos",Conversoes.Executar },
+                {"Operações Aritméticos - Fundamentos",OperacoesAritmeticos.Executar },
+                {"Operações Relacionais - Fundamentos",OperacoesRelacionais.Executar },
+                {"Operações Lógicos - Fundamentos",OperadoresLogicos.Executar },
+                {"Operações Atribuição - Fundamentos",OperadoresAtribuicao.Executar },
+                {"Operações Unários - Fundamentos",OperadoresUnarios.Executar },
+                {"Operador Tenários - Fundamentos",OperadorTernario.Executar },
+
+                //Estrutura de Controle
+                {"Estrutura IF - Estruturas de Controle",EstruturaIf.Executar },
+                {"Estrutura If/Else - Estruturas de Controle",EstruturaIfElse.Executar },
+                {"Estrutura If/Else/If - Estruturas de Controle",EstruturaIfElseIf.Executar },
+                {"Estrutura Switch - Estruturas de Controle",EstruturaSwitch.Executar },
+                {"Estrutura While - Estruturas de Controle",EstruturaWhile.Executar },
+                {"Estrutura Do While - Estruturas de Controle",EstruturaDoWhile.Executar },
+                {"Estrutura For - Estruturas de Controle",EstruturaFor.Executar },
+                {"Estrutura ForEach - Estruturas de Controle",EstruturaForEach.Executar },
+                {"Usando Break - Estruturas de Controle",UsandoBreak.Executar },
+                {"Usando Continue - Estruturas de Controle",UsandoContinue.Executar },
+                
+                // Classes e Métodos
+                {"Membros - Classes e Métodos",Membros.Executar },
+                {"Construtores - Classes e Métodos",Construtores.Executar },
+                {"Métodos com Retorno - Classes e Métodos",MetodosComRetorno.Executar },
+                {"Métodos Estáticos - Classes e Métodos",MetodosEstaticos.Executar },
+                {"Atributos Estáticos - Classes e Métodos",AtributosEstaticos.Executar },
+                {"Desafio Atributo - Classes e Métodos",DesafioAtributo.Executar },
+                {"Params - Classes e Métodos",Params.Executar },
+                {"Parametros Nomeados - Classes e Métodos",ParametrosNomeados.Executar },
+                {"Get Set - Classes e Métodos",GetSet.Executar },
+                {"Props - Classes e Métodos",Props.Executar },
+
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
