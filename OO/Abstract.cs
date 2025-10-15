7b51a99946084c4f8020b8676e261710ace70236
Matheus Domingues <59287764+Matheus-D@users.noreply.github.com>
2025-10-15 13:33:24 -0400

    Coleções de OO

--- OO/Abstract.cs
+++ OO/Abstract.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.OO
 {
+    public abstract class Celular {
+        public abstract string Assistente();
+
+        public string Tocar() { 
+            return "Trim, trim, trim...";
+        }
+    }
+
+    public class Samsung : Celular {
+        public override string Assistente() {
+            return "Ola! Meu nome é Bixby!";
+        }
+    }
+
+    public class Iphone : Celular {
+        public override string Assistente() {
+            return "Olá! Meu nome é Siri";
+        }
+    }
     class Abstract
     {
         public static void Executar() {
-            
+            var celulares = new List<Celular>() {
+                new Iphone(), 
+                new Samsung()
+            };
+
+            foreach (var celular in celulares) {
+                Console.WriteLine(celular.Assistente());  
+            }
         }
     }
 }
